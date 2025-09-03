60d464857dad65495563be92157b5d10a6136c3d
o-az <omaraziz.dev@proton.me>
2025-09-03 02:05:36 -0700

    first commit

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+import 'tsx/cjs'
 import type { ExpoConfig, ConfigContext } from 'expo/config'
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
@@ -5,10 +6,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   slug: 'porto-rn',
   name: 'porto-rn',
   version: '1.0.0',
-  expo: {
-    jsEngine: 'hermes',
-    ios: {
-      jsEngine: 'jsc',
-    },
+  jsEngine: 'hermes',
+  ios: {
+    jsEngine: 'jsc',
   },
 })

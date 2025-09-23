4b6bb479196f1b56d02af30dc1582007ec95e7b4
o-az <omaraziz.dev@proton.me>
2025-09-23 08:40:49 -0700

    chore: verified iOS, Android & Web work

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -4,15 +4,25 @@ import Constants from 'expo-constants'
 import type { Address, Hex } from 'ox'
 import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
+import * as AuthSession from 'expo-auth-session'
 import * as SafeAreaContext from 'react-native-safe-area-context'
 import { Button, Text, View, StyleSheet, Platform } from 'react-native'
+
+if (Platform.OS === 'web') WebBrowser.maybeCompleteAuthSession()
 
 const useIsomorphicLayoutEffect =
   typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect
 
 const portoBaseUrl = `${process.env.EXPO_PUBLIC_PORTO_BASE_URL || 'https://id.porto.sh'}/dialog`
 /**
- * @see https://docs.expo.dev/versions/latest/sdk/webbrowser/#webbrowseropenauthsessionasyncurl-redirecturl-options
+ * @see
+ * - https://docs.expo.dev/guides/authentication/#rules-for-all-authentication-providers
+ * - https://docs.expo.dev/versions/latest/sdk/webbrowser/#webbrowseropenauthsessionasyncurl-redirecturl-options
+ *
+ * TODO:
+ * - [platform:ios,android]
+ *   once there's a session-based authentication, we'll use `expo-secure-store`
+ *   to store the value
  */
 
 export default function Tab() {
@@ -21,34 +31,10 @@ export default function Tab() {
   const [authSessionStatus, setAuthSessionStatus] = React.useState<
     'success' | 'error' | 'idle'
   >('idle')
+
+  const [authError, setAuthError] = React.useState<string | null>(null)
   const [address, setAddress] = React.useState<string | null>(null)
   const [publicKey, setPublicKey] = React.useState<string | null>(null)
-
-  const { success, payload, parsedPayload } = React.useMemo(() => {
-    if (!linkingUrl)
-      return { success: null, payload: null, parsedPayload: null }
-    const url = new URL(linkingUrl)
-    const success = url.searchParams.get('success')
-    const payload = url.searchParams.get('payload')
-    const parsedPayload = payload ? (JSON.parse(payload) as Payload) : null
-    const address = parsedPayload?.accounts[0]?.address ?? null
-    const publicKey =
-      parsedPayload?.accounts[0]?.capabilities.admins[0]?.publicKey ?? null
-    setAddress(address)
-    setPublicKey(publicKey)
-    return {
-      success,
-      payload,
-      parsedPayload: payload ? (JSON.parse(payload) as Payload) : null,
-    } as {
-      success: string | null
-      payload: string | null
-      parsedPayload: Payload | null
-    }
-  }, [linkingUrl])
-
-  const [authSessionResult, setAuthSessionResult] =
-    React.useState<WebBrowser.WebBrowserAuthSessionResult | null>(null)
 
   useIsomorphicLayoutEffect(() => {
     if (Platform.OS === 'android') WebBrowser.warmUpAsync()
@@ -58,7 +44,7 @@ export default function Tab() {
   }, [])
 
   async function handleDeepLink(event: Linking.EventType) {
-    const { url, nativeEvent } = event
+    const { url } = event
     const initialUrl = await Linking.getInitialURL()
     console.info('initialUrl', initialUrl)
     const parsedUrl = Linking.parse(url)
@@ -69,15 +55,9 @@ export default function Tab() {
     }
     if (queryParams && Object.hasOwn(queryParams, 'success')) {
       console.info('queryParams', queryParams)
+      if (['web', 'ios'].includes(Platform.OS)) WebBrowser.dismissAuthSession()
     }
-    if (['web', 'ios'].includes(Platform.OS)) WebBrowser.dismissAuthSession()
   }
-
-  useIsomorphicLayoutEffect(() => {
-    console.info('handleDeepLink')
-    const subscription = Linking.addEventListener('url', handleDeepLink)
-    return () => subscription.remove()
-  }, [])
 
   async function handleAuthSessionPress(options: { createAccount: boolean }) {
     const params = [
@@ -89,23 +69,27 @@ export default function Tab() {
       },
     ]
 
-    const redirectUrl =
-      Platform.OS === 'web'
-        ? `https://${process.env.EXPO_PUBLIC_TUNNEL_SUBDOMAIN}.ngrok.io/auth`
-        : `porto-rn://auth`
+    // const redirectUri =
+    //   Platform.OS === 'web'
+    //     ? `https://${process.env.EXPO_PUBLIC_TUNNEL_SUBDOMAIN}.ngrok.io/auth`
+    //     : `porto-rn://auth`
+
+    const redirectUri = AuthSession.makeRedirectUri({
+      path: 'auth',
+    })
+    console.info('requestUri', redirectUri)
 
     const searchParams = new URLSearchParams({
-      id: '1',
-      jsonrpc: '2.0',
       relayEnv: 'prod',
-      redirectUrl,
+      redirectUri,
       method: 'wallet_connect',
+      os: Platform.OS.toLowerCase(),
       params: encodeURIComponent(JSON.stringify(params)),
       _decoded: encodeURIComponent(JSON.stringify(params)),
     })
     const result = await WebBrowser.openAuthSessionAsync(
       `${portoBaseUrl}/wallet_connect?${searchParams.toString()}`,
-      redirectUrl,
+      redirectUri,
       {
         showTitle: false,
         showInRecents: false,
@@ -116,16 +100,25 @@ export default function Tab() {
       },
     )
 
+    if (!Object.hasOwn(result, 'url')) return
+
     const url = new URL((result as any).url)
+    console.info('result.url', url)
     const error = (result as any).error
     const success = url.searchParams.get('success')
+    const status = url.searchParams.get('status')
+    if (status !== 'success') {
+      const message = url.searchParams.get('message')
+      setAuthError(`${status} - ${message}`)
+      return
+    }
     const payload = url.searchParams.get('payload')
-    console.info('url', url)
     console.info('error', error)
-    console.info('success', success)
+    console.info('status', status)
     console.info('payload', payload)
 
-    if (success === 'true') {
+    if (success === 'true' || status === 'success') {
+      console.info('result.success', success)
       const parsedPayload = payload ? (JSON.parse(payload) as Payload) : null
       const address = parsedPayload?.accounts[0]?.address ?? null
       const publicKey =
@@ -135,10 +128,18 @@ export default function Tab() {
     }
 
     setAuthSessionStatus(result.type === 'success' ? 'success' : 'error')
-    setAuthSessionResult(result)
 
     if (Platform.OS === 'web') WebBrowser.maybeCompleteAuthSession()
   }
+
+  useIsomorphicLayoutEffect(() => {
+    console.info('handleDeepLink')
+    const subscription = Linking.addEventListener('url', handleDeepLink)
+    return () => {
+      subscription.remove()
+    }
+  }, [])
+
   return (
     <SafeAreaContext.SafeAreaView style={styles.container}>
       <View
@@ -153,6 +154,7 @@ export default function Tab() {
         <Text style={styles.text}>AuthSession Status: {authSessionStatus}</Text>
         {address && <Text style={styles.text}>Address: {address}</Text>}
         {publicKey && <Text style={styles.text}>Public Key: {publicKey}</Text>}
+        {authError && <Text style={styles.text}>Error: {authError}</Text>}
       </View>
       <Button
         title="AuthSession (create account)"
@@ -219,6 +221,12 @@ function Events() {
       <Text>{JSON.stringify(responses, null, 2)}</Text>
     </View>
   )
+}
+
+type RedirectSearchParams = {
+  payload: string
+  message: string
+  status: 'success' | 'error' | 'cancel' | 'unknown'
 }
 
 type Payload = {

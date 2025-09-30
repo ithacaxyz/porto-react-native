96131e06666c4c6a37410f09fefbdcb70eca1bcd
o-az <omaraziz.dev@proton.me>
2025-09-30 15:04:44 -0700

    chore: save

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -4,20 +4,12 @@ import type { Address, Hex } from 'ox'
 import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
 import * as AuthSession from 'expo-auth-session'
-import { NitroText } from 'react-native-nitro-text'
 import * as SafeAreaContext from 'react-native-safe-area-context'
-import {
-  Button,
-  View,
-  StyleSheet,
-  Platform,
-  Text as WebText,
-} from 'react-native'
+import { Button, View, StyleSheet, Platform, Text } from 'react-native'
 
 if (Platform.OS === 'web') WebBrowser.maybeCompleteAuthSession()
 
-const Text = Platform.OS === 'web' ? WebText : NitroText
-
+// https://localhost:5174/dialog/wallet_connect?relayEnv=prod&_decoded=%7B%22method%22%3A%22wallet_connect%22%2C%22params%22%3A%5B%7B%22capabilities%22%3A%7B%22createAccount%22%3Atrue%2C%22email%22%3Atrue%2C%22grantPermissions%22%3A%7B%22expiry%22%3A1759006129%2C%22feeToken%22%3A%7B%22limit%22%3A%221%22%2C%22symbol%22%3A%22EXP%22%7D%2C%22permissions%22%3A%7B%22calls%22%3A%5B%7B%22to%22%3A%220x2d49a0e75c86779C391418214eC7e1B18E58Bb34%22%7D%2C%7B%22to%22%3A%220x7C61733E8a9c6Dac20afeB46e9c4ba96C5A9f7cf%22%7D%2C%7B%22signature%22%3A%22mint%28%29%22%2C%22to%22%3A%220xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef%22%7D%5D%2C%22spend%22%3A%5B%7B%22limit%22%3A%2250000000000000000000%23__bigint%22%2C%22period%22%3A%22minute%22%2C%22token%22%3A%220x2d49a0e75c86779C391418214eC7e1B18E58Bb34%22%7D%5D%7D%7D%2C%22signInWithEthereum%22%3A%7B%22chainId%22%3A421614%2C%22nonce%22%3A%22deadbeef%22%7D%7D%7D%5D%7D&id=0&method=wallet_connect&params=%5B%7B%22capabilities%22%3A%7B%22createAccount%22%3Atrue%2C%22email%22%3Atrue%2C%22grantPermissions%22%3A%7B%22expiry%22%3A1759006129%2C%22feeToken%22%3A%7B%22limit%22%3A%221%22%2C%22symbol%22%3A%22EXP%22%7D%2C%22key%22%3A%7B%22publicKey%22%3A%220x649d97ddb05a80e88772413e0a48d903bc440c8b4e7fd1997e194cf1d0c7704465f394f826f3a61d866134f9827a9f054a184dd5c2c2c8c2cf87405fc28d3e98%22%2C%22type%22%3A%22p256%22%7D%2C%22permissions%22%3A%7B%22calls%22%3A%5B%7B%22to%22%3A%220x2d49a0e75c86779C391418214eC7e1B18E58Bb34%22%7D%2C%7B%22to%22%3A%220x7C61733E8a9c6Dac20afeB46e9c4ba96C5A9f7cf%22%7D%2C%7B%22signature%22%3A%22mint%28%29%22%2C%22to%22%3A%220xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef%22%7D%5D%2C%22spend%22%3A%5B%7B%22limit%22%3A%220x2b5e3af16b1880000%22%2C%22period%22%3A%22minute%22%2C%22token%22%3A%220x2d49a0e75c86779C391418214eC7e1B18E58Bb34%22%7D%5D%7D%7D%2C%22signInWithEthereum%22%3A%7B%22chainId%22%3A421614%2C%22nonce%22%3A%22deadbeef%22%7D%7D%7D%5D&jsonrpc=2.0
 const useIsomorphicLayoutEffect =
   typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect
 
@@ -84,6 +76,8 @@ export default function Tab() {
       relayEnv: 'prod',
       redirectUri,
       method: 'wallet_connect',
+      id: '0',
+      jsonrpc: '2.0',
       os: Platform.OS.toLowerCase(),
       params: encodeURIComponent(JSON.stringify(params)),
       _decoded: encodeURIComponent(JSON.stringify(params)),
